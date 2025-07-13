3e301b9ac8d328c105caaced0f30193137ce2355
NavithaGoud <peddinavithagoud@gmail.com>
2025-07-13 09:13:50 +0530

    url encoded modified

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const path = require("path")
 let app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use('/profilePics', express.static('profilePics'));
 app.use(express.static(path.join(__dirname,"./client/build")));
 
